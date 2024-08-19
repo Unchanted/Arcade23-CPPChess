210b9bf7358a7a17b5782ecb3094496fb57af845
Unchanted <omkkdwivedi@gmail.com>
2024-08-20 00:52:49 +0530

    restructuring

--- source/obs_rec.cpp
+++ source/obs_rec.cpp
@@ -1,4 +1,3 @@
-    
 #include <ncurses.h>
 #include <cmath>
 #include "../headers/calc.h"
@@ -6,47 +5,35 @@
 #include "../headers/main.h"
 #include "../headers/write.h"
 
-// check wether there is something in the path
+// Check whether there is something in the path
 // if the piece is moving straight or not
 bool check_straight(char from[], char to[]) {
     int init, end;
     char cord;
-    // check weter it is moving straight in the y axis
+
+    // Determine the axis of movement (straight in y or x axis)
     if (y_cord(from) == y_cord(to)) {
-        // check wether it is moving left or right
-        if (x_cord(from) > x_cord(to)) {
-            init = x_cord(to);
-            end = x_cord(from);
-        } else {
-            init = x_cord(from);
-            end = x_cord(to);
-        }
         cord = 'x';
-        // check weter it is moving straight in the x axis
+        init = std::min(x_cord(from), x_cord(to));
+        end = std::max(x_cord(from), x_cord(to));
     } else if (x_cord(from) == x_cord(to)) {
-        // check wether it is moving upward or downward
-        if (y_cord(from) > y_cord(to)) {
-            init = y_cord(to);
-            end = y_cord(from);
-        } else {
-            init = y_cord(from);
-            end = y_cord(to);
-        }
         cord = 'y';
+        init = std::min(y_cord(from), y_cord(to));
+        end = std::max(y_cord(from), y_cord(to));
+    } else {
+        return true; // Not moving straight
     }
-    // checking every block on the way from intiial
-    // postion to the end piostion for blank pieces or not
-    for (size_t i = init + 1; i < end; i++) {
-        // check through y axis if its moving through y axis
+
+    // Check each block along the path for any obstacles
+    for (int i = init + 1; i < end; i++) {
         if (cord == 'x') {
             if (map[y_cord(from) - 1][i - 1][0] != '-') {
-                write(input, WOR_PAIR, 7, 1, "Someting on the way");
+                write(input, WOR_PAIR, 7, 1, "Something on the way");
                 return false;
             }
-            // ceck thorugh x axis if its moveing through xx axis
         } else if (cord == 'y') {
             if (map[i - 1][x_cord(from) - 1][0] != '-') {
-                write(input, WOR_PAIR, 7, 1, "SOmething on the way");
+                write(input, WOR_PAIR, 7, 1, "Something on the way");
                 return false;
             }
         }
@@ -54,52 +41,25 @@ bool check_straight(char from[], char to[]) {
     return true;
 }
 
-// check wehter there is something along the path
-//  depending if there is any piece in the path or not
+// Check whether there is something along the path
+// depending if there is any piece in the path or not
 bool check_diagnol(char from[], char to[]) {
-    int x_init, x_end, y_init, y_end;
+    int x_step, y_step;
     size_t x, y;
 
-    // check if its moving diagnoally or not
+    // Check if it's moving diagonally
     if (abs(y_cord(from) - y_cord(to)) == abs(x_cord(from) - x_cord(to))) {
-        // check if its moving upward or downward
-        if (y_cord(from) > y_cord(to) && x_cord(from) > x_cord(to)) {
-            for (x = x_cord(to) + 1, y = y_cord(to) + 1;
-                 x < x_cord(from), y < y_cord(from); x++, y++) {
-                if (map[y - 1][x - 1][0] != '-') {
-                    write(input, WOR_PAIR, 7, 1, "Someting on the way");
-                    return false;
-                }
-            }
-        } else if (y_cord(from) < y_cord(to) && x_cord(from) < x_cord(to)) {
-            for (x = x_cord(from) + 1, y = y_cord(from) + 1;
-                 x < x_cord(to), y < y_cord(to); x++, y++) {
-                if (map[y - 1][x - 1][0] != '-') {
-                    write(input, WOR_PAIR, 7, 1, "Someting on the way");
-                    return false;
-                }
-            }
-        } else if (y_cord(from) < y_cord(to) && x_cord(from) > x_cord(to)) {
-            for (x = x_cord(from) + 1, y = y_cord(from) + 1;
-                 x < x_cord(to), y < y_cord(to); x--, y++) {
-                if (map[y - 1][x - 1][0] != '-') {
-                    write(input, WOR_PAIR, 7, 1, "Someting on the way");
-                    return false;
-                }
-            }
-        } else if (y_cord(from) > y_cord(to) && x_cord(from) < x_cord(to)) {
-            for (x = x_cord(from) + 1, y = y_cord(from) + 1;
-                 x < x_cord(to), y < y_cord(to); x++, y--) {
-                if (map[y - 1][x - 1][0] != '-') {
-                    write(input, WOR_PAIR, 7, 1, "Someting on the way");
-                    return false;
-                }
+        x_step = (x_cord(from) < x_cord(to)) ? 1 : -1;
+        y_step = (y_cord(from) < y_cord(to)) ? 1 : -1;
+
+        // Check each block along the diagonal path for any obstacles
+        for (x = x_cord(from) + x_step, y = y_cord(from) + y_step;
+             x != x_cord(to); x += x_step, y += y_step) {
+            if (map[y - 1][x - 1][0] != '-') {
+                write(input, WOR_PAIR, 7, 1, "Something on the way");
+                return false;
             }
         }
-
-        // looping trhought the path and check every block for some piece
-        // making x and y as their inital position and looping until the end
-        // position
     }
     return true;
 }
